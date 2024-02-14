5a31e0f548b1e2556838ded182c5c05d1310d8ae
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-02-14 13:15:26 +0530

    spell

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express =require('express')
 const app=express()
-
+const core =require('cors')
+const coreConfig={
+    origin:"*",
+    credential:true,
+    methods:["GET", "POST", "PUT","DELETE"]
+}
+app.options("",core(coreConfig))
+app.use(core(coreConfig))
 app.set('view engine', 'ejs')
 app.get('/',(req,resp)=>{
     resp.render("index")
